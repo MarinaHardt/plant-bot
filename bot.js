9c06f815ba4986f212793d7cbbb62e6c8d1623f0
MarinaHardt <marina.hardt-mitidieri@stud.h-da.de>
2021-01-03 15:04:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot("1373145412:AAG7OeGNQE1-4AHore4nGRPQI9aHhfNnduw", {
   polling: true, // receiving messages
 });
-const request = require("request"); // for HTTP requests
+const request = require("request"); // for HTTP requestsbot.js
 const trefleToken = "YrWtVX-fUjuH-ISKU7_TnjOsKMZMG56EmJrlqGllANs";
 
 // syntax /search "some plant"
 bot.onText(/\/search (.+)/, (msg, match) => {
-  let plant = match[1];
+  let plant = match[1]; // match "some plant"
   let chatId = msg.chat.id; // telegram message
   request(
     `https://trefle.io/api/v1/species/search?token=${trefleToken}&q=${plant}`,
@@ -17,7 +17,7 @@ bot.onText(/\/search (.+)/, (msg, match) => {
         bot
           // set up promise
           .sendMessage(chatId, `_We're looking for ${plant}..._`, {
-            parse_mode: "Markdown",
+            parse_mode: "Markdown", // for italics
           })
           .then((msg) => {
             let res = JSON.parse(body); // convert received data (string) a JSON object
@@ -26,7 +26,7 @@ bot.onText(/\/search (.+)/, (msg, match) => {
               .sendPhoto(chatId, res.data[0].image_url, {
                 caption: `${res.data[0].scientific_name} \nAlso know as ${res.data[0].common_name}, it is a part of the ${res.data[0].family} family. Its name was first published in ${res.data[0].year}.\nThere are ${res.meta.total} other plants under this name. Maybe you might also be looking for ${res.data[1].common_name}/${res.data[1].scientific_name}?`,
               })
-              // Error message when rejecting promise
+              // Error message when there's no match for "some plant"
               .catch((err) => {
                 if (err) {
                   bot.sendMessage(chatId, "Plant not found, please try again!");
